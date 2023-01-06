a128f097c9069897c530614743ff4f438c1fde2c
Arnaud <arnaudjourdain@orange.fr>
2023-01-06 15:31:44 +0100

    update Text edit and link subscribe

--- app/src/main/java/com/grp2/a4al2androidgrp2/MainActivity.kt
+++ app/src/main/java/com/grp2/a4al2androidgrp2/MainActivity.kt
@@ -1,11 +1,18 @@
 package com.grp2.a4al2androidgrp2
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    fun showNewLayout(view: View) {
+        val intent = Intent(this, SubscribeActivity::class.java)
+        startActivity(intent)
     }
 }
---
+++ app/src/main/java/com/grp2/a4al2androidgrp2/SubscribeActivity.java
@@ -0,0 +1,6 @@
+package com.grp2.a4al2androidgrp2;
+
+import android.app.Activity;
+
+public class SubscribeActivity extends Activity {
+}
