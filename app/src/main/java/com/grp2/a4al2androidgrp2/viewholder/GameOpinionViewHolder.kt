25d97d7fb8ed92731e12747e1aa8ba282811ac15
Arnaud <arnaudjourdain@orange.fr>
2023-01-31 21:35:28 +0100

    opinion sexy

--- app/src/main/java/com/grp2/a4al2androidgrp2/viewholder/GameOpinionViewHolder.kt
+++ app/src/main/java/com/grp2/a4al2androidgrp2/viewholder/GameOpinionViewHolder.kt
@@ -2,6 +2,7 @@ package com.grp2.a4al2androidgrp2.viewholder
 
 import android.text.Html
 import android.view.View
+import android.widget.LinearLayout
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.grp2.a4al2androidgrp2.R
@@ -12,5 +13,10 @@ class GameOpinionViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) 
     fun bind(gameOpinion: GameOpinion) {
         itemView.findViewById<TextView>(R.id.user_name).text = Html.fromHtml("<u>${gameOpinion.author.pseudo}</u>")
         itemView.findViewById<TextView>(R.id.content).text = Html.fromHtml(gameOpinion.review)
+        if (gameOpinion.voted_up) {
+            itemView.findViewById<LinearLayout>(R.id.good_opinion).visibility = View.VISIBLE
+        } else {
+            itemView.findViewById<LinearLayout>(R.id.bad_opinion).visibility = View.VISIBLE
+        }
     }
 }
